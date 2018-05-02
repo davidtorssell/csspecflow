939e01de7fe1237afc630e2fdbc9629a3d988947
davidtorssell <david.torssell@gmail.com>
2018-05-02 10:30:59 +0200

    first commit

---
+++ SpecFlowProject/BogusFeature.feature
@@ -0,0 +1,10 @@
+﻿Feature: POC
+    In order to show myself off as a badass test automator
+    I want to implement a test framework 
+    in a language I have never worked with before
+	
+	
+@bogusTag
+Scenario: Basic stuff up and running
+    Given I have navigated to "http://www2.hm.com/sv_se/index.html"
+    Then I should be able to see the link to the shopping cart
---
+++ SpecFlowProject/TestSteps.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject
+{
+    [Binding]
+    public class TestSteps {
+
+        IWebDriver driver = new ChromeDriver();
+
+        [Given(@"I have navigated to ""(.*)""")]
+        public void GivenIHaveNavigatedTo(string p0)
+        {
+            driver.Url = p0;
+        }
+
+
+        [Then(@"I should be able to see the link to the shopping cart")]
+        public void ThenIShouldBeAbleToSeeTheLinkToTheShoppingCart()
+        {
+            IWebElement shoppingCart = driver.FindElement(By.CssSelector(".minicart.parbahse"));
+        }
+
+
+    }
+
+
+
+}
